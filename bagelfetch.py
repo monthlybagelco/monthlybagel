48fb2e880da6404e7d0c078c8a4811b2fd7e69ca
Russell Strauss <straussrussell05@gmail.com>
2017-12-28 10:55:22 -0700

    updates - apache2 server compatability

--- bagelfetch.py
+++ bagelfetch.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python
-import urllib
-url = "http://rawgit.com/monthlybagelco/monthlybagel/master/bagel.html"
-f = urllib.urlopen(url)
-print f.read()
+#!/usr/bin/python3
+import urllib.request
+txt = ""
+url = "https://raw.githubusercontent.com/monthlybagelco/monthlybagel/master/bagel.html"
+f = urllib.request.urlopen(url)
+txt = txt + f.read().decode("utf-8")
+print(txt)
